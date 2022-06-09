5c59fd6519c97f2dc22c138233256660e9b011df
Aidyn Omarov <aidyn_o_b@mail.ru>
2022-06-09 13:18:09 +0600

    check this code

--- students_test.go
+++ students_test.go
@@ -7,6 +7,7 @@ import (
 	"reflect"
 	"errors"
 	"strconv"
+	"fmt"
 )
 
 // DO NOT EDIT THIS FUNCTION
@@ -23,125 +24,168 @@ func init() {
 
 // WRITE YOUR CODE BELOW
 
-func TestLen(t *testing.T) {
-	var people People
+var PersonList People = []Person{
+	{
+		firstName: "Ivan",
+		lastName:  "Drago",
+		birthDay:  time.Date(1985, time.May, 25, 0, 0, 0, 0, time.UTC),
+	},
+	{
+		firstName: "Olexa",
+		lastName:  "Кvitka",
+		birthDay:  time.Date(1995, time.December, 05, 0, 0, 0, 0, time.UTC),
+	},
+	{
+		firstName: "Olexa",
+		lastName:  "Drobush",
+		birthDay:  time.Date(1995, time.December, 05, 0, 0, 0, 0, time.UTC),
+	},
+	{
+		firstName: "John",
+		lastName:  "Doe",
+		birthDay:  time.Date(1995, time.December, 05, 0, 0, 0, 0, time.UTC),
+	},
+}
 
-	if people.Len() != 0 {
-		t.Errorf("Wrong People Len empty")
-	}
+func TestPeople_Len(t *testing.T) {
+	Expected := 4 // who knows whether we need here len(PersonList) or not :)
+	got := PersonList.Len()
 
-	people = append(people, Person{})
-	people = append(people, Person{})
-	people = append(people, Person{})
-
-	if people.Len() != 3 {
-		t.Errorf("Wrong People Len 3")
-	}
-
-	people = people[0:1]
-
-	if people.Len() != 1 {
-		t.Errorf("Wrong People Len 1")
+	if got != Expected {
+		t.Errorf("Expected %d, got %d", Expected, got)
 	}
 }
 
-func TestLess(t *testing.T) {
-	var people People
-	now := time.Time{}
-
-	people = append(people, Person{"BBB", "BBB", now})
-	people = append(people, Person{"BBB", "BBB", now.Add(5 * time.Minute)})
-	people = append(people, Person{"AAA", "BBB", now})
-	people = append(people, Person{"AAA", "AAA", now})
-	people = append(people, Person{"AAA", "AAA", now})
-
-	if people.Less(0, 1) {
-		t.Errorf("Wrong People Less by Birthday")
+func TestPeople_Less(t *testing.T) {
+	tData := map[string]struct {
+		FirstPersonIndex  int
+		SecondPersonIndex int
+		Expected          bool
+	}{
+		"diffDates":             {0, 1, false},
+		"sameDateDiffNames":     {1, 3, false},
+		"sameDateSameFirstName": {1, 2, false},
 	}
 
-	if people.Less(0, 2) {
-		t.Errorf("Wrong People Less by FirstName")
-	}
-
-	if people.Less(2, 3) {
-		t.Errorf("Wrong People Less by LastName")
-	}
-
-	if people.Less(3, 4) {
-		t.Errorf("Wrong People Less Eq")
+	for name, v := range tData {
+		t.Run(name, func(t *testing.T) {
+			got := PersonList.Less(v.FirstPersonIndex, v.SecondPersonIndex)
+			if got != v.Expected {
+				t.Errorf("[%s] expected: %v, got %v", name, v.Expected, got)
+			}
+		})
 	}
 }
 
-func TestSwap(t *testing.T) {
-	var people People
-	now := time.Time{}
-
-	people1 := Person{"first", "first", now}
-	people2 := Person{"first", "first", now}
-
-	people = append(people, people1)
-	people = append(people, people2)
-
-	people.Swap(0, 1)
-
-	if people[0] != people2 || people[1] != people1 {
-		t.Errorf("Wrong People Swap")
+func TestPeople_Swap(t *testing.T) {
+	tData := struct {
+		FirstPeopleInd  int
+		SecondPeopleInd int
+		Expected        Person
+	}{
+		0,
+		1,
+		PersonList[0],
+	}
+	PersonList.Swap(tData.FirstPeopleInd, tData.SecondPeopleInd)
+	got := PersonList[tData.SecondPeopleInd]
+	if got != tData.Expected {
+		t.Errorf("Expected %v, got %v", tData.Expected, got)
 	}
 }
+
+/////////////////////////////////////////////////////////////////////
 
 func TestNew(t *testing.T) {
-	actual, err := New("---")
-	if actual != nil || !errors.Is(err, strconv.ErrSyntax) {
-		t.Errorf("Wrong String Error")
+	tData := map[string]struct {
+		TestString string
+		Expected   *Matrix
+		Err        error
+	}{
+		"ProperData": {
+			"1 1 1\n2 2 2",
+			&Matrix{2, 3, []int{1, 1, 1, 2, 2, 2}},
+			nil,
+		},
+		"DiffRowsLength": {
+			"1 1 1\n2 2 2 2",
+			nil,
+			errors.Unwrap(fmt.Errorf("Rows need to be the same length")), // this is strange
+			// could not find better solution
+		},
+		"ImproperData": {
+			"a",
+			nil,
+			strconv.ErrSyntax,
+		},
 	}
 
-	actual, err = New("1 1 \n 2")
-	if actual != nil || err.Error() != "Rows need to be the same length" {
-		t.Errorf("Wrong Matrix Error")
-	}
-
-	actual, err = New("1 1 \n 2 3")
-	expects := &Matrix{2, 2, []int{1, 1, 2, 3}}
-
-	if actual.cols != expects.cols || actual.rows != expects.rows || !reflect.DeepEqual(actual.data, expects.data) {
-		t.Errorf("Wrong Empty Matrix")
-	}
-}
-
-func TestRows(t *testing.T) {
-	matrix := &Matrix{2, 2, []int{4, 5, 6, 7}}
-	expects := [][]int{{4, 5}, {6, 7}}
-
-	actual := matrix.Rows()
-
-	if !reflect.DeepEqual(actual, expects) {
-		t.Errorf("Wrong Rows Matrix")
-	}
-}
-
-func TestCols(t *testing.T) {
-	matrix := &Matrix{3, 3, []int{1, 10, 100, 2, 20, 200, 3, 30, 300}}
-	expects := [][]int{{1, 2, 3}, {10, 20, 30}, {100, 200, 300}}
-
-	actual := matrix.Cols()
-
-	if !reflect.DeepEqual(actual, expects) {
-		t.Errorf("Wrong Cols Matrix")
+	for name, v := range tData {
+		t.Run(name, func(t *testing.T) {
+			got, err := New(v.TestString)
+			if err != nil && !errors.Is(errors.Unwrap(err), v.Err) { // could not find better solution, think
+				//  the error message from the New() needs some changes for better logic
+				t.Errorf("[%s] error expected:\"%v\", got:\"%v\".\n", name, v.Err, err)
+			}
+			if !reflect.DeepEqual(v.Expected, got) { // needs to be remembered when compare structs with slices&maps
+				t.Errorf("[%s] expected: %v, got %v", name, v.Expected, got)
+			}
+		})
 	}
 }
 
-func TestSet(t *testing.T) {
-	matrix := &Matrix{3, 3, []int{1, 10, 100, 2, 20, 200, 3, 30, 300}}
-	expectsData := []int{1, 10, 100, 2, 20, 200, 3, 30, 10000000}
+func TestMatrix_Rows(t *testing.T) {
+	tData := struct {
+		TestMatrix *Matrix
+		Expected   [][]int
+	}{
+		&Matrix{2, 3, []int{1, 1, 1, 2, 2, 2}}, // should I use New() here?
+		[][]int{{1, 1, 1}, {2, 2, 2}},
+	}
+	got := tData.TestMatrix.Rows()
+	if !reflect.DeepEqual(tData.Expected, got) {
+		t.Errorf("Expected %v, got %v", tData.Expected, got)
+	}
+}
 
-	actual := matrix.Set(2, 2, 10000000)
+func TestMatrix_Cols(t *testing.T) {
+	tData := struct {
+		TestMatrix *Matrix
+		Expected   [][]int
+	}{
+		&Matrix{2, 3, []int{1, 1, 1, 2, 2, 2}}, // should I use New() here?
+		[][]int{{1, 2}, {1, 2}, {1, 2}},
+	}
+	got := tData.TestMatrix.Cols()
+	if !reflect.DeepEqual(tData.Expected, got) {
+		t.Errorf("Expected %v, got %v", tData.Expected, got)
+	}
+}
 
-	if !actual || !reflect.DeepEqual(matrix.data, expectsData) {
-		t.Errorf("Wrong Set Matrix")
+func TestMatrix_Set(t *testing.T) {
+	baseMatrix := &Matrix{2, 3, []int{1, 1, 1, 2, 2, 2}}
+	needMatrixT := &Matrix{2, 3, []int{1, 1, 1, 2, 5, 2}}
+	needMatrixF := &Matrix{2, 3, []int{1, 1, 1, 2, 2, 2}}
+
+	tData := map[string]struct {
+		row            int
+		col            int
+		value          int
+		ExpectedMatrix *Matrix
+		Expected       bool
+	}{
+		"ProperData":     {1, 1, 5, needMatrixT, true},
+		"RowLessO":       {-1, 1, 5, baseMatrix, false},
+		"ImproperRowNum": {2, 1, 5, needMatrixF, false},
+		"ImproperColNum": {1, 3, 5, needMatrixF, false},
 	}
 
-	actual = matrix.Set(-1, 2, 10000000)
-	if actual {
-		t.Errorf("Wrong Set Error")
+	for name, v := range tData {
+		t.Run(name, func(t *testing.T) {
+			got := baseMatrix.Set(v.row, v.col, v.value)
+			if got != v.Expected && !reflect.DeepEqual(v.ExpectedMatrix, baseMatrix) {
+				t.Errorf("[%s] expected: %v, got %v", name, v.Expected, got)
+			}
+		})
 	}
 }
